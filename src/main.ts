2d779a3e972f6ad981085841bbabed01151a8dd6
John Pangalos <john@pangalos.dev>
2023-09-23 22:03:58 +0200

    More viewport modification

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="flex items-center justify-center h-full">
     <button id="shoot"
       type="button"
-      class="bg-pink-200 text-gray-700 text-3xl rounded-full w-44 h-44 font-bold border-gray-700 border-2">
+      class="bg-pink-200 text-gray-700 text-3xl rounded-full w-44 h-44 font-bold border-gray-700 border-2 select-none">
       Woohoo!
     </button>
   </div>
