b1c11d44556eaa3c99e5f935f24f1abae57badcb
John Pangalos <john@pangalos.dev>
2023-10-02 07:20:30 +0200

    Fire!

--- src/confetti.ts
+++ src/confetti.ts
@@ -1,36 +1,22 @@
-import { confetti } from "tsparticles-confetti";
-// import horseAudio from "./horse.mp3";
-import yeahboyAudio from "./yeah-boy.mp3";
-import amongus from "./amongus.png";
+import { loadFirePreset } from "tsparticles-preset-fire";
+import { tsParticles } from "tsparticles-engine";
+import screamAudio from "./scream.mp3";
 
-const defaults = {
-  spread: 360,
-  ticks: 100,
-  gravity: 0,
-  decay: 0.94,
-  startVelocity: 30,
-};
 async function shoot() {
-  await confetti({
-    ...defaults,
-    particleCount: 20,
-    scalar: 2,
-    shapes: ["images"],
-    shapeOptions: {
-      images: {
-        src: amongus,
-        width: 50,
-        height: 64,
+  await loadFirePreset(tsParticles);
+  await tsParticles.load("tsparticles", {
+    particles: {
+      shape: {
+        type: "square", // starting from v2, this require the square shape script
       },
     },
+    preset: "fire",
   });
 }
 
-// const horse = new Audio(horseAudio);
-
 export function setupShoot(element: HTMLButtonElement) {
-  const yeahboy = new Audio(yeahboyAudio);
   element.addEventListener("click", async () => {
-    await Promise.all([shoot(), yeahboy.play()]);
+    const scream = new Audio(screamAudio);
+    await Promise.all([shoot(), scream.play()]);
   });
 }
